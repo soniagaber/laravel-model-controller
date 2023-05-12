e0b6a370c025b9fbc84497ed446d4b27c681f1af
soniagaber <soniagaber90@gmail.com>
2023-05-12 19:12:17 +0200

    finito: i film vengono visualizzati in una tabella

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,28 @@
 @extends('layouts/app')
 
 @section('content')
-
+<table class="table table-dark">
+    <thead>
+      <tr>
+        <th scope="col">TITLE</th>
+        <th scope="col">ORIGINAL TITLE</th>
+        <th scope="col">NATIONALITY</th>
+        <th scope="col">DATE</th>
+        <th scope="col">VOTE</th>
+      </tr>
+    </thead>
+    <tbody>
+        @foreach ($movies as $movie)
+        <tr>
+            <td>{{$movie->title}}</td>
+            <td>{{$movie->original_title}}</td>
+            <td>{{$movie->nationality}}</td>
+            <td>{{$movie->date}}</td>
+            <td>{{$movie->vote}}</td>
+        </tr>
+            
+        @endforeach
+    </tbody>
+  </table>
 @endsection
+
